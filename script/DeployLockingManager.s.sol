0d5c09ec8423f6719ef41895ed3baf545b1c1b3e
kitanovicd <kitanovic.danilo@outlook.com>
2024-12-10 09:57:19 +0100

    feat: deploy stCHI/ETH LP locking manager

--- script/DeployLockingManager.s.sol
+++ script/DeployLockingManager.s.sol
@@ -4,14 +4,15 @@ pragma solidity ^0.8.0;
 import {Script, console} from "forge-std/Script.sol";
 import {LockingManager} from "src/tokenomics/LockingManager.sol";
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {DeployConstants} from "./library/DeployConstants.sol";
 
 contract DeployLockingManager is Script {
     address public constant PROXY_ADMIN = 0x347451E2BC19CB63E6A370d5Ab8d09591B8704Ea;
     address public constant INITIAL_OWNER = 0xE36fF60a9Ae677c2d742a3DeddCA46d0fA999327;
 
     // TODO: Change this parameters every time you deploy a new contract
-    address public constant LOCKING_TOKEN = 0x044bCdf7deA1a825B7be24573b738462a4FE9D3f;
-    address public constant REWARD_TOKEN = 0x3b21418081528845a6DF4e970bD2185545b712ba; // CHI
+    address public constant LOCKING_TOKEN = 0x8f3871fD26Ac117f6E3D55E5f98E627Ca5d5e581;
+    address public constant REWARD_TOKEN = DeployConstants.CHI;
     uint256 public constant EPOCH_START_TIME = 1733696402;
     uint256 public constant EPOCH_DURATION = 1 days;
     uint256 public constant INITIAL_REWARDS_PER_EPOCH = 10_000 * 1e18;
