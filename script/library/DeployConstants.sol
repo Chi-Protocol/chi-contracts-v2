e1f18335f72ecef8b79ea0309e84c8a90967d174
Danilo Kitanovic <116795362+kitanovicd@users.noreply.github.com>
2024-12-10 13:12:04 +0100

    feat: deploy stCHI/ETH LP locking manager (#9)

---
+++ script/library/DeployConstants.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library DeployConstants {
+    address internal constant CHI = 0x3b21418081528845a6DF4e970bD2185545b712ba;
+    address internal constant USC = 0x38547D918b9645F2D94336B6b61AEB08053E142c;
+}
